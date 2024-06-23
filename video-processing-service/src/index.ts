aec27a04440c2c39f9d82ed0935e8229827e2131
edoski <87473803+edoski@users.noreply.github.com>
2024-06-23 18:51:51 +0200

    working video processing (locally)

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,14 +1,36 @@
-import express from 'express';
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+    //   GET THE VIDEO FILE PATH FROM THE REQUEST
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send(
+            'BAD REQUEST: MISSING FILE PATH FOR ' + inputFilePath === undefined ? 'INPUT' : 'OUTPUT'
+        );
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360P RESOLUTION
+        .on("end", () => {
+            console.log("VIDEO PROCESSING COMPLETED");
+            res.status(200).send("VIDEO PROCESSING COMPLETED");
+        })
+        .on("error", (err) => {
+            console.log(`AN ERROR OCCURRED: ${err.message}`)
+            res.status(500).send(`INTERNAL SERVER ERROR: ${err.message}`);
+        })
+        .save(outputFilePath)
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(
-      'video-processing-service listening at http://localhost:' + port
-  );
+    console.log(
+        'video-processing-service listening at http://localhost:' + port
+    );
 });
